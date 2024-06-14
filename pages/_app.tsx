cb1f60d2517747876940ab85b84ac592d73bee48
CallumBoase <callum.boase@gmail.com>
2024-06-15 09:37:35 +1000

    Auto compare htmls

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,28 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 //Import the CSS required for SupabaseUppyUploader globally
 // import "@uppy/core/dist/style.min.css";
 // import "@uppy/dashboard/dist/style.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    // Capture the client-side HTML after initial render
+    const clientHtml = document.documentElement.outerHTML;
+
+    if (typeof global.serverHtml !== 'undefined') {
+      // Compare the HTML strings
+      if (global.serverHtml !== clientHtml) {
+        console.error('Hydration Error: HTML mismatch detected');
+        // console.log('Server HTML:', global.serverHtml);
+        // console.log('Client HTML:', clientHtml);
+      }
+    }
+  }, []);
+
+
+
   return <Component {...pageProps} />;
 }
 
---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    // Capture the rendered HTML
+    if (typeof window === 'undefined') {
+      // console.log('Server-Side HTML:', initialProps.html);
+      global.serverHtml = initialProps.html;
+    }
+
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
